1e7e3e95ef7707a54161d2960e2caa92ab46e540
pampels <gist.jr.pampellona@gmail.com>
2013-12-19 18:14:35 +0800

    Added carts and defined method to fine current cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218091955) do
+ActiveRecord::Schema.define(version: 20131219101249) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
