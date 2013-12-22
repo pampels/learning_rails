f4b753ab3b0e5e303534e8b7917c4d2d6d303be3
pampels <gist.jr.pampellona@gmail.com>
2013-12-22 21:56:58 +0800

    Added AJAX functionality to Adding Item in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -36,6 +36,7 @@ class LineItemsController < ApplicationController
         session[:views] = 0
 
         format.html { redirect_to store_url}
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
