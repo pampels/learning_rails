52887e8cbee7280de560b5c663c4463581cab59a
pampels <gist.jr.pampellona@gmail.com>
2013-12-22 20:31:00 +0800

    Moved the cart to application layout and created patials for cart and line_item

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.all
+  	@cart = current_cart
   	@num_views = increment_views
   	@views_html = "(You've been here #{@num_views} times)" if @num_views > 5
   end
