8af7382c07b24af43a6c0380e9ed3f8172d2827b
pampels <gist.jr.pampellona@gmail.com>
2013-12-23 13:27:31 +0800

    Fixed errors and failures in unit/functionals tests

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,8 @@ class LineItemsControllerTest < ActionController::TestCase
     end
 
     # assert_redirected_to line_item_path(assigns(:line_item))
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    # assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
@@ -46,6 +47,17 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, id: @line_item
     end
 
-    assert_redirected_to line_items_path
+    # assert_redirected_to line_items_path
+    assert_redirected_to store_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count' ) do
+      xhr :post, :create, :product_id => products(:ruby).id
+    end
+    assert_response :success
+    assert_select_jquery :html do
+      assert_select 'tr.updated_item td' , /Programming Ruby 1.9/
+    end
   end
 end
